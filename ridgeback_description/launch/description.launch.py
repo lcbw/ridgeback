fa3ef9280ea5d711a37a7d965fb7171912a0adb2
LCBW <lcbw99@gmail.com>
2023-05-01 16:26:44 -0500

    cleaning for proper loading

--- ridgeback_description/launch/description.launch.py
+++ ridgeback_description/launch/description.launch.py
@@ -23,7 +23,7 @@ from ament_index_python.packages import get_package_share_directory
 def generate_launch_description():
   this_share_directory = get_package_share_directory('ridgeback_description')
   ridgeback_control_directory = get_package_share_directory('ridgeback_control')
-  xacro_path = os.path.join(this_share_directory, 'urdf', 'ridgeback.urdf.xacro')
+  xacro_path = os.path.join(get_package_share_directory('ridgeback_description'), 'urdf', 'ridgeback.urdf.xacro')
 
   # Launch configuration variables specific to simulation
   use_sim_time = LaunchConfiguration('use_sim_time')
@@ -41,7 +41,7 @@ def generate_launch_description():
 
   declare_config_cmd = DeclareLaunchArgument(
     'config',
-    default_value=os.getenv('ridgeback_CONFIG', 'base'),
+    default_value=os.getenv('RIDGEBACK_CONFIG', 'base'),
     description='get the ridgeback configuration')
 
   declare_physical_robot_cmd = DeclareLaunchArgument(
@@ -49,23 +49,25 @@ def generate_launch_description():
     default_value='true',
     description='Whether or not you run it on the physical robot')
 
-  declare_physical_robot_cmd = DeclareLaunchArgument(
-     'physical_robot',
+  declare_robot_ip_cmd = DeclareLaunchArgument(
+     'robot_ip',
      default_value='192.168.131.1',
      description='IP address for the ridgeback in XXX.XXX.XXX.XXX format')
 
-  declare_physical_robot_cmd = DeclareLaunchArgument(
-    'control_yaml_file',
-    default_value=ridgeback_control_directory+'/config/control.yaml',
-    description='The config file for gazebo_ros2_control, only needed in simulation')
+#  declare_control_yaml_file = DeclareLaunchArgument(
+#    'control_yaml_file',
+#    default_value=ridgeback_control_directory+'/config/control.yaml',
+#    description='The config file for gazebo_ros2_control, only needed in simulation')
 
-  robot_description = Command([
-    os.path.join(this_share_directory, 'env_run'),
-    ' ',
-    os.path.join(this_share_directory, 'urdf', 'configs', 'base'),
-    ' ',
-    'xacro',' ', xacro_path, ' ', 'physical_robot:=', physical_robot, ' ', 'control_yaml_file:=', control_yaml_file
-  ])
+#  robot_description = Command([
+#    os.path.join(this_share_directory, 'env_run'),
+#    ' ',
+#    os.path.join(this_share_directory, 'urdf', 'configs', 'base'),
+#    ,' ',
+#    'xacro',' ', xacro_path, ' ', 'physical_robot:=', physical_robot, ' ', 'control_yaml_file:=', control_yaml_file
+#  ])
+
+  robot_description_parameter = {"robot_description": Command(['xacro',' ', xacro_path, ' ', 'physical_robot:=', physical_robot])}
 
   # Specify the actions
   start_robot_state_publisher_cmd = Node(
@@ -75,7 +77,7 @@ def generate_launch_description():
     output='screen',
     parameters=[{
       'use_sim_time': use_sim_time,
-      'robot_description': robot_description
+      'robot_description': robot_description_parameter
     }]
   )
 
@@ -85,6 +87,8 @@ def generate_launch_description():
   ld.add_action(declare_use_sim_time_cmd)
   ld.add_action(declare_config_cmd)
   ld.add_action(declare_physical_robot_cmd)
+  ld.add_action(declare_robot_ip_cmd)
+#  ld.add_action(declare_control_yaml_file)
 
   # Add any conditioned actions
   ld.add_action(start_robot_state_publisher_cmd)
